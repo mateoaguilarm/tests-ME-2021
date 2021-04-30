7d30323bce52340f699c364d953c7c49ee19cc2f
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-04-29 20:21:48 -0500

    some order, nav added

--- 002-react-test/src/App.js
+++ 002-react-test/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Bootstrap from './components/Bootstrap';
 import Reactstrap from './components/Reactstrap';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Switch>
           <Route path='/bootstrap' component={Bootstrap}/>
           <Route path='/reactstrap' component={Reactstrap}/>
