af48f2757e5976095a5708e3f7365fd1e793f2b2
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-06-10 18:10:49 -0500

    Setting 1sts parts with instructions - setted nodemon

--- Backend Tests/001bcknd/index.js
+++ Backend Tests/001bcknd/index.js
@@ -1,2 +1,17 @@
+// Parte 1, Inicializando Express
 const express = require('express');
 const app = express();
+
+
+
+// Parte 2, Configurando la primera parte de Express (Middlewares)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Parte 4, 
+// app.use('/api/v1', router)
+
+// Parte 3, Levantando la app
+app.listen(4000, function () {
+    console.log('server is on');
+})
