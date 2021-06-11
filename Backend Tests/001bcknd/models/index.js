bbf44aad6ae131d10be711fff518ee1ed3db70af
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-06-10 20:35:52 -0500

    mongodb part setted 1/4

--- Backend Tests/001bcknd/models/index.js
+++ Backend Tests/001bcknd/models/index.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+const MONGO_URI = 'mongodb+srv://user-test:1234@backend.65lwy.mongodb.net/001bcknd?retryWrites=true&w=majority';
+
+const Article = require('./Article');
+
+mongoose.connect(
+    MONGO_URI, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (error) => {
+        error ? console.log(error) : console.log('Database Connected');
+    },
+);
+
+module.exports = {
+    Article,
+};
