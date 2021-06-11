c561ee40b140a8f765cd9eaef4401ca3f7b8c85b
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-06-10 19:31:11 -0500

    introducing mongoose by installing and adding models folder

--- Backend Tests/001bcknd/index.js
+++ Backend Tests/001bcknd/index.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const app = express();
 
-
+const router = require('./routes')
 
 // Parte 2, Configurando la primera parte de Express (Middlewares)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Parte 4, 
-// app.use('/api/v1', router)
+app.use('/api/v1', router)
 
 // Parte 3, Levantando la app
 app.listen(4000, function () {
---
+++ Backend Tests/001bcknd/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (_, response) => {
+    response.status(200).send('Hello world');
+});
+
+
+
+module.exports = router;
